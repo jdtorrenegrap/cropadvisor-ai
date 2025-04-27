b46349d6b0c1d240c29c94ebced3fadbe802ac6f
Jesus Torrenegra <jdtrorrenegrap@ul.edu.co>
2025-04-27 17:02:11 -0500

    the function is corrected to send image plus user message

--- src/models/chat_message.py
+++ src/models/chat_message.py
@@ -1,7 +1,7 @@
-# filepath: /home/jsust/Desktop/cropadvisor/src/models/chat_message.py
+from typing import Optional
 from pydantic import BaseModel
 
 class ChatMessage(BaseModel):
-    type: str = "text" 
-    message: str = None
-    base64: str = None
+    type: str = "text"
+    message: Optional[str] = None
+    base64: Optional[str] = None

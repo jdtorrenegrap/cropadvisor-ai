2379a3912b6260ec888f95c18665c2a2d26a3e9a
Jesus Torrenegra <jdtrorrenegrap@ul.edu.co>
2025-05-14 19:49:13 -0500

    A new llm model is implemented, design patterns are applied, and improved yolo models are integrated.

--- test/stream.py
+++
@@ -1,8 +0,0 @@
-from google import genai
-
-client = genai.Client(api_key="AIzaSyDxcjO9Reb4PlIwy14GWoUCVk2Fa6PYpt8")
-
-response = client.models.generate_content(
-    model="gemini-2.0-flash", contents="Hola"
-)
-print(response.text)
